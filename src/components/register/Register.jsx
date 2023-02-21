1f56ec509c4f0940376369ea6c6daaa5d4cc3195
kalidasm <kalidasm@zero-pixels.com>
2023-02-21 16:27:51 +0530

    Change in Register API

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -7,9 +7,61 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Register = () => {
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      name: "",
+      username: "",
+      password: "",
+      submit: null,
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name is required"),
+      username: Yup.string()
+        .email("email must be vaild")
+        .required("email is required"),
+      password: Yup.string()
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`#^(|{}"'@$!%&])[\d~#-}"!]{8,20}$/,
+          "Password format must match the standard password security guidelines. Password must be between 8 and 20 characters, contain an upper case letter, lower case letter, number, and special character."
+        )
+        .required("Password is required"),
+    }),
+    onSubmit: async (values) => {
+      debugger;
+      try {
+        const data = {
+          name: values.name,
+          username: values.username,
+          password: values.password,
+        };
+        await fetch(
+          "https://zpworkshopapis.netlify.app/.netlify/functions/account/signup",
+          {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // setBlogs(data);
+            alert("User Registered");
+            // setPopupOpen(true);
+            navigate("/login", { replace: true });
+          });
+        // postUser(regData);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  });
   return (
     <Box sx={{ display: "flex" }}>
       <Box
@@ -42,7 +94,7 @@ export const Register = () => {
             }}
           >
             <form
-              onSubmit={(event) => event.preventDefault()}
+              onSubmit={formik.handleSubmit}
               style={{ width: "100%", maxWidth: "80%" }}
             >
               <Box sx={{ display: "flex", alignItems: "center", mb: 5 }}>
@@ -89,41 +141,93 @@ export const Register = () => {
               <Grid container spacing={3} sx={{ mt: 0.5 }}>
                 <Grid item md={6} xs={12}>
                   <TextField
+                    error={Boolean(formik.touched.name && formik.errors.name)}
+                    helperText={formik.touched.name && formik.errors.name}
                     fullWidth
-                    label="Full name"
+                    label="Name"
+                    value={formik.values.name}
                     name="name"
+                    onBlur={formik.handleBlur}
+                    onChange={(e) => {
+                      formik.handleChange(e);
+                    }}
                     required
-                    value=""
                   />
                 </Grid>
                 <Grid item md={6} xs={12}>
                   <TextField
-                    fullWidth
-                    label="Email address"
-                    name="email"
                     required
-                    value=""
+                    fullWidth
+                    error={Boolean(
+                      formik.touched.username && formik.errors.username
+                    )}
+                    helperText={
+                      formik.touched.username && formik.errors.username
+                    }
+                    variant="outlined"
+                    label="Username"
+                    name="username"
+                    placeholder="enter your email address"
+                    onBlur={formik.handleBlur}
+                    onChange={(e) => {
+                      formik.handleChange(e);
+                    }}
+                    value={formik.values.username}
+                    sx={{
+                      borderColor: "neutral.300",
+                    }}
                   />
+                  {/* <TextField
+                    fullWidth
+                    error={Boolean(
+                      formik.touched.username && formik.errors.username
+                    )}
+                    helperText={
+                      formik.touched.username && formik.errors.username
+                    }
+                    label="Email address"
+                    name="username"
+                    required
+                    value={formik.values.username}
+                    onChange={(e) => {
+                      formik.handleChange(e);
+                    }}
+                  /> */}
                 </Grid>
                 <Grid item md={6} xs={12}>
                   {" "}
                   <TextField
+                    error={Boolean(
+                      formik.touched.password && formik.errors.password
+                    )}
+                    helperText={
+                      formik.touched.password && formik.errors.password
+                    }
                     fullWidth
                     label="Password"
                     name="password"
-                    value=""
+                    value={formik.values.password}
+                    onBlur={formik.handleBlur}
+                    onChange={(e) => {
+                      formik.handleChange(e);
+                    }}
+                    required
                   />
                 </Grid>
                 <Grid />
-                <Grid item md={6} xs={12}>
+                {/* <Grid item md={6} xs={12}>
                   {" "}
                   <TextField
                     fullWidth
                     label="Confirm Password"
-                    name="conpassword"
-                    value=""
+                    name="confrirmPassword"
+                    value={formik.values.confrirmPassword}
+                    onChange={(e) => {
+                      formik.handleChange(e);
+                    }}
+                    required
                   />
-                </Grid>
+                </Grid> */}
                 <Grid />
               </Grid>
               <Box sx={{ mt: 6 }}>
